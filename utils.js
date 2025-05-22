e45085d68ddb1dc0b57f1bb9f880f5012b4c09f8
kimnerd <hyuntaec@naver.com>
2025-05-22 21:55:23 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 export function latLonAltToVec3(lon, lat, alt) {
   const phi = (90 - lat) * Math.PI / 180;
   const theta = (-lon) * Math.PI / 180;
-  const radius = 1 + alt;
+  const radius = 1 + alt * 3; // 고도 확대 계수 3배
 
   return new THREE.Vector3(
     radius * Math.sin(phi) * Math.cos(theta),
